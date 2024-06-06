887e3aad3d65a7ddc64f0cfb0708190a577215a6
Azarias Galama <129897944+azariasabera@users.noreply.github.com>
2024-06-06 15:28:18 +0300

    Modified my last answer so that the list takes text from the textarea.

--- code.js
+++ code.js
@@ -1,20 +1,25 @@
 const myButton = document.getElementById('my-button');
+
 myButton.addEventListener('click', function() {
+
     console.log('hello world');
     let text = "Moi maailma";
     document.getElementById('my-text').innerText = text;
+
 });
 
 const addButton = document.getElementById('add-data');
 
 addButton.addEventListener('click', function() {
+
 let newLi = document.createElement('li');
-newLi.innerText = 'This is a new text!';
+newLi.innerText = document.getElementById('my-textarea').value;
 document.getElementById('my-list').appendChild(newLi);
-document.getElementById('my-textarea').innerText = newLi.innerText;
+
+});
+
 
 /*
 Shortly line 11 - 13 can be written as:
-document.getElementById('my-list').innerHTML += '<li>This is a new text!</li>';
+document.getElementById('my-list').innerHTML += '<li>This is a new text! or sth</li>';
 */
-});
